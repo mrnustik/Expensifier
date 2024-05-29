d645def5445b0419fe1f544bcfecb4d92c276461
Michal Mrnustik <michal.mrnustik@outlook.com>
2024-05-29 20:15:26 +0200

    Add basic react routing

---
+++ client/src/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { isRouteErrorResponse, useRouteError } from "react-router-dom"
+
+
+export const ErrorPage: React.FC = () => {
+    const error = useRouteError();
+    console.error(error);
+
+    let errorMessage: string;
+
+    if (isRouteErrorResponse(error)) {
+      // error is type `ErrorResponse`
+      errorMessage = error.data.message || error.statusText;
+    } else if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === 'string') {
+      errorMessage = error;
+    } else {
+      console.error(error);
+      errorMessage = 'Unknown error';
+    }
+
+    return (
+        <div>
+            <h1>An error occurred</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{errorMessage}</i>
+            </p>
+        </div>
+    )
+} 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { AccountList } from './Accounts/AccountsList';
+import { ErrorPage } from './ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AccountList />,
+    errorElement: <ErrorPage />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
