5ba90b3553151c8b387669cb1e50872b5e87f53a
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-02-23 18:30:10 +0100

    chore: remove extra line in Root

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -5,7 +5,6 @@ import { Outlet } from "react-router";
 export const Root: React.FC = () => {
     return <>
         <div>
-
             <Box sx={{ display: 'flex' }}>
                 <AppBar position='absolute'>
                     <Toolbar>
