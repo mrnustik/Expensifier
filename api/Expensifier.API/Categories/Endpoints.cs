b4fa2dfbfcb02dfc90caf59012e4d3d7d90df9c7
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-02-23 22:18:09 +0100

    feat: add categories management

---
+++ api/Expensifier.API/Categories/Endpoints.cs
@@ -0,0 +1,28 @@
+﻿using Expensifier.API.Categories.CreateCategory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expensifier.API.Categories;
+
+public static class Endpoints
+{
+    public static void MapCategoryEndpoints(this WebApplication app)
+    {
+        app.MapPost("/api/categories", 
+            async ([FromBody]CreateCategoryCommand command, 
+                IMediator mediator,
+                CancellationToken ct) =>
+            {
+                var categoryId = await mediator.Send(command, ct);
+                return TypedResults.Created($"api/categories/{categoryId}", categoryId);
+            });
+
+        app.MapGet("/api/categories",
+                   async (IMediator mediator,
+                          CancellationToken ct) =>
+                   {
+                       var categories = await mediator.Send(new GetCategories.GetCategories(), ct);
+                       return TypedResults.Ok(categories);
+                   });
+    }
+}
---
+++ api/Expensifier.API/Categories/GetCategories/GetCategories.cs
@@ -0,0 +1,30 @@
+﻿using Expensifier.API.Common.Queries;
+using Expensifier.API.Common.Users;
+using Marten;
+
+namespace Expensifier.API.Categories.GetCategories;
+
+public record GetCategories : Query<IReadOnlyCollection<Category>>
+{
+    public class Handler : QueryHandler<GetCategories, IReadOnlyCollection<Category>>
+    {
+        private readonly IQuerySession _querySession;
+        private readonly IUserProvider _userProvider;
+
+        public Handler(IQuerySession querySession, IUserProvider userProvider)
+        {
+            _querySession = querySession;
+            _userProvider = userProvider;
+        }
+
+        protected override async Task<IReadOnlyCollection<Category>> Query(
+            GetCategories query,
+            CancellationToken cancellationToken)
+        {
+            var categories = await _querySession.Query<Category>()
+                                                .Where(c => c.UserId == _userProvider.CurrentUserId)
+                                                .ToListAsync(cancellationToken);
+            return categories.ToList();
+        }
+    }
+}
